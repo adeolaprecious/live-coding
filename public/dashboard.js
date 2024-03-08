68f603aacd886362cd0e130f7c74df05e2023a6b
Adebowale Adeola Precious <adeolaprecious006@gmail.com>
2024-03-08 12:18:27 +0100

    uykyu

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+// import { getDatabase, ref, set, onValue, push  } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// import { getStorage, ref as stref} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBNZ5rMQONMni6sLvNEyIX19Ss96Tuax_0",
@@ -7,35 +9,75 @@ const firebaseConfig = {
     projectId: "live-coding-7fce1",
     storageBucket: "live-coding-7fce1.appspot.com",
     messagingSenderId: "908767745762",
-    appId: "1:908767745762:web:5fa66fdd08466f0d0ef0bb"
+    appId: "1:908767745762:web:5fa66fdd08466f0d0ef0bb",
+    databaseURL:"https://live-coding-7fce1-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+// const storage = getStorage()
+// const database = getDatabase(app);
 
-// function signO =() = {
-//     signOut(auth).then(() => {
-//         // Sign-out successful.
-//     }).catch((error) => {
-//         // An error happened.
-//     });
-// }
 
-function submitTodo(){
-    alert("hi")
-    let inputOne= document.getElementById("todo").value
-    let inputTwo =document.getElementById("description").valuesh
+function submitTodo() {
+    let inputOne = document.getElementById("todo").value
+    let inputTwo = document.getElementById("description").value
     let date = new Date
     console.log(date);
-    let object = {inputOne, inputTwo, date}
+    let object = { inputOne, inputTwo, date }
     console.log(object);
-    if(inputOne==='' || inputTwo===''){
-        view.style.display="block"
-    }else{
-        show.innerHTML
+    // let dbRef = push(ref(database, `table`))
+    // let saved = set(dbRef, object)
+    // if (saved) {
+    //     enter.style.display = "block"
+    //     console.log("Saved successfully");
+    // } else {
+    //     console.log("failed to save");
+    // }
+    if (inputOne === '' || inputTwo === '') {
+        view.style.display = "block"
+    } else {
+        table.splice
+        view.style.display = "none"
+        show.innerHTML = `<div class="border border-2 shadow-sm mx-">
+                            <h2><label for="">Todo:</label></h2>
+                                <h3>${inputOne}</h3>
+                            <h2><label for="">Todo Description:</label></h2>
+                                <h3>${inputTwo}</h3>
+                            <p> ${date}</p>
+                        <div>
+                    `
     }
+    document.getElementById("todo").value = ""
+    document.getElementById("description").value = ""
 }
-window.submitTodo=submitTodo
+window.submitTodo = submitTodo
+// let newRef = ref(database, `table`)
+// onValue(newRef, (snapshot) => {
+//     let data = snapshot.val()
+//     console.log(data);
+//     show.innerHTML =""
+//         for(let key in data){
+//         if(data.hasOwnProperty(key)){
+//             let todo = data[key];
+            
+//             show.innerHTML += `
+//             <h3 class="text-center">${todo.inputOne}</h3>
+//             <h5 class="text-center" >${todo.inputTwo}</h5>
+//             <p class="text-center">${todo.time}, ${todo.date}</p>
+
+
+//         `
+//         }
+//     }
+// })
+
+// function deleteAll() {
+//     itemarray.splice(0, itemarray.length);
+//     cart()
+// }
+
+
 
 
