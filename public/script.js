3f363e4fe0b1e787447eae0146997cb74f5701dd
Adebowale Adeola Precious <adeolaprecious006@gmail.com>
2024-03-08 10:34:05 +0100

    commit 1

---
+++ public/script.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth,GoogleAuthProvider,  signInWithPopup, } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNZ5rMQONMni6sLvNEyIX19Ss96Tuax_0",
+    authDomain: "live-coding-7fce1.firebaseapp.com",
+    projectId: "live-coding-7fce1",
+    storageBucket: "live-coding-7fce1.appspot.com",
+    messagingSenderId: "908767745762",
+    appId: "1:908767745762:web:5fa66fdd08466f0d0ef0bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
